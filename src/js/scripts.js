3575e55c1312392a993d18a89997eb5d2f98b98f
Mrs. Krabs <47456460+MasterKrab@users.noreply.github.com>
2021-02-03 21:40:17 -0300

    finish

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,83 +1,162 @@
+
+// Selectors
 const form = document.getElementById("to-do-form")
 const optionContainer = document.getElementById("to-do-options")
 const tasksContainer = document.getElementById("tasks-list")
 const options = document.querySelectorAll(".to-do__option")
+const eraseButton = document.getElementById("erase-button")
 
+//Variables
 let tasks = []
 let task
+let complete = false
 
-const generateID = () => {
-   Math.random().toString(36).substr(2, 9)
+// Fragment
+const fragment = document.createDocumentFragment()
+
+// Functions
+
+   // LocalStorage
+const syncStorage = () =>{
+   localStorage.setItem("tasks", JSON.stringify(tasks))
+}
+   // Function to generate a unique id
+const generateID = () => Math.random().toString(36).substr(2, 9)
+
+   // Filter to completed tasks
+const filterCompleted = () =>{
+   complete = true
+   printHTML(tasks.filter(task => task.finish))
+}
+   // Filter to active tasks
+const filterActive = () =>{
+   complete = false
+   printHTML(tasks.filter(task => !task.finish))
 }
 
+
 const printHTML = (tasks) =>{
-   tasks.forEach(task =>{
-      const {id, task} = task
-      console.log(id)
-      console.log(task)
+
+   tasksContainer.textContent = ""
+
+   tasks.forEach(taskObj =>{
+      const {id, task, finish} = taskObj
+      
 
       const li = document.createElement("LI")
       li.classList.add("task")
-      li.innerHTML = `
-      <div class="task__check">
+      const div = document.createElement("DIV")
+      div.classList.add("task__check")
+      div.innerHTML =`
          <input class="task__input" type="checkbox" id="${id}">
-         <label  class="task__label"  for="task1323">${task}.</label>
-      </div>
-
-      <i class="fas fa-trash task__erase task__erase--active"></i>
+         <label  class="task__label"  for="${id}">${task}.</label>
       `
-      tasksContainer.appendChild(li)
+      if(finish) div.firstElementChild.checked = true
+      li.appendChild(div)
       
+      const i = document.createElement("I")
+      i.classList.add("fas", "fa-trash", "task__erase")
+      if(complete) {
+         eraseButton.classList.add("erase-button--active")
+         i.classList.add("task__erase--active")
+      }else{
+         eraseButton.classList.remove("erase-button--active")
+      }
+      i.dataset.id = id
+      li.appendChild(i)
+
+      fragment.appendChild(li)
    })
+   tasksContainer.appendChild(fragment)
 }
 
 const addTask = task =>{
-   
-      console.log(task)
 
       const taskObj = {
          id: generateID(),
-         task
+         task: task,
+         finish: false,
       }
 
-      tasks = [...tasks, task]
+      tasks = [...tasks, taskObj]
 
-      console.log(tasks)
-      printHTML(tasks)
-   
+      printHTML(tasks)  
+      syncStorage()
 }
 
-
-
+// EventListeners
 
 optionContainer.addEventListener("click", e =>{
    if(e.target.classList.contains("to-do__option")) {
       options.forEach(options => options.classList.remove("to-do__option--active"));
       e.target.classList.add("to-do__option--active")
+
+      if(e.target.dataset.id == 1) {
+         complete = false
+         printHTML(tasks)
+      }
+
+      e.target.dataset.id == 2 && filterActive()
+      e.target.dataset.id == 3 && filterCompleted()
+      
    }
-   
 })
 
 form.addEventListener("submit", e =>{
    e.preventDefault()
 
    task = form.task.value
-   console.log(task)
 
    if(task.trim().length > 0) {
-      console.log(task)
 
-      const taskObj = {
-         id: generateID(),
-         task
-      }
-
-      tasks = [...tasks, task]
-
-      console.log(tasks)
-      printHTML(tasks)
+      addTask(task)
       
+      options.forEach(option => {
+         option.classList.remove("to-do__option--active")
+         if(option.dataset.id == "1" && !option.classList.contains("to-do__option--active")) { 
+            option.classList.add("to-do__option--active")
+            complete = false
+            printHTML(tasks)
+         }
+      })
       form.reset()
    }
+})
+
+tasksContainer.addEventListener("click", e =>{
+   if(e.target.checked){
+      tasks.forEach(task =>{
+         if(task.id == e.target.id)
+         task.finish = true
+      })
+   }else{
+      tasks.forEach(task =>{
+         if(task.id == e.target.id)
+         task.finish = false
+      })
+   }
+
+   if(e.target.classList.contains("task__erase")){
+      tasks = tasks.filter(task => task.id !== e.target.dataset.id)
+      filterCompleted()
+   }
+   
+   syncStorage()
+})
+
+// Delete all button
+eraseButton.addEventListener("click", () =>{
+   tasks = []
+   printHTML(tasks)
+   eraseButton.classList.remove("erase-button--active")
+
+   syncStorage()
+})
+
+// Localstorage 
+document.addEventListener("DOMContentLoaded", ()=>{
+   tasks = JSON.parse(localStorage.getItem("tasks")) || []
+
+   printHTML(tasks)
 
 })
